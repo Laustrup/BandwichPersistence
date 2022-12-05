372dc4968ef1c64ec36d5c4abc6e6909b7ccde85
Laust <laust.bonnesen@mail.com>
2022-12-05 17:40:10 +0100

    Endpoint for assembling Event implemented

--- src/main/java/laustrup/bandwichpersistence/controllers/EventController.java
+++ src/main/java/laustrup/bandwichpersistence/controllers/EventController.java
@@ -1,7 +1,23 @@
 package laustrup.bandwichpersistence.controllers;
 
+import laustrup.bandwichpersistence.models.events.Event;
+import laustrup.bandwichpersistence.services.controller_services.EventControllerService;
+import laustrup.bandwichpersistence.utilities.Liszt;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/event/")
 public class EventController {
+
+    @PostMapping("get/{id}")
+    public ResponseEntity<Event> get(@PathVariable(name = "id") long id) {
+        return EventControllerService.get_instance().get(id);
+    }
+    @PostMapping("get")
+    public ResponseEntity<Liszt<Event>> get() {
+        return EventControllerService.get_instance().get();
+    }
 }
--- src/main/java/laustrup/bandwichpersistence/services/controller_services/EventControllerService.java
+++ src/main/java/laustrup/bandwichpersistence/services/controller_services/EventControllerService.java
@@ -1,5 +1,12 @@
 package laustrup.bandwichpersistence.services.controller_services;
 
+import laustrup.bandwichpersistence.models.events.Event;
+import laustrup.bandwichpersistence.models.users.User;
+import laustrup.bandwichpersistence.services.persistence_services.assembling_services.Assembly;
+import laustrup.bandwichpersistence.utilities.Liszt;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class EventControllerService {
 
     /**
@@ -18,4 +25,41 @@ public class EventControllerService {
     }
 
     private EventControllerService() {}
+
+    /**
+     * Creates a ResponseEntity for a controller to send to client.
+     * This scenario is for getting an Event by its id.
+     * Uses an assemblyService for reading the database and building the Event object.
+     * @param id The id of the Event, that is wished to be gathered.
+     * @return The created ResponseEntity of an Event.
+     */
+    public ResponseEntity<Event> get(long id) { return entityContent(Assembly.get_instance().getEvent(id)); }
+
+    /**
+     * Creates a ResponseEntity for a controller to send to client.
+     * This scenario is for getting all Events.
+     * Uses an assemblyService for reading the database and building the Event objects.
+     * @return The created ResponseEntity of all Events.
+     */
+    public ResponseEntity<Liszt<Event>> get() { return entityContent(Assembly.get_instance().getEvents()); }
+
+    /**
+     * Will create a ResponseEntity with status of whether the content is null or not.
+     * @param event The Event that is either null or not and should be returned.
+     * @return The created ResponseEntity of an Event.
+     */
+    private ResponseEntity<Event> entityContent(Event event) {
+        if (event != null) return new ResponseEntity<>(event, HttpStatus.OK);
+        else return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * Will create a ResponseEntity with status of whether the content is null or not.
+     * @param events The Events that is either null or not and should be returned.
+     * @return The created ResponseEntity of Events.
+     */
+    private ResponseEntity<Liszt<Event>> entityContent(Liszt<Event> events) {
+        if (events != null) return new ResponseEntity<>(events, HttpStatus.OK);
+        else return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
 }
