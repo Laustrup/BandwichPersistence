77244beec43d6c39f57ff8ea5a961578fd39a4ce
Laustrup <laust.bonnesen@mail.com>
2025-03-24 10:09:16 +0100

    SecurityConfiguration simplified and cleaner

--- src/main/java/laustrup/bandwichpersistence/web/configuration/SecurityConfiguration.java
+++ src/main/java/laustrup/bandwichpersistence/web/configuration/SecurityConfiguration.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -22,17 +25,24 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request ->
-                        request
-//                                .requestMatchers("/user/login").permitAll()
-//                                .requestMatchers("/user/create").permitAll()
-                                .anyRequest().permitAll()
-                )
+                .authorizeHttpRequests(SecurityConfiguration::configureRequests)
 //                .logout(Customizer.withDefaults())
 //                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                ).build();
+                .sessionManagement(SecurityConfiguration::configureSessionManagement).build();
+    }
+
+    private static <T extends HttpSecurityBuilder<T>> void configureRequests(
+            AuthorizeHttpRequestsConfigurer<T>.AuthorizationManagerRequestMatcherRegistry registry
+    ) {
+        registry
+                .anyRequest()
+                .permitAll();
+    }
+
+    private static <T extends HttpSecurityBuilder<T>> void configureSessionManagement(
+            SessionManagementConfigurer<T> configurer
+    ) {
+        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
